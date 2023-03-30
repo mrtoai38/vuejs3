656e3ff1b544dec8eb2d1af421b48f9fa2fbff67
mrtoai38 <toaihuu19122000@gmail.com>
2023-03-30 16:32:25 +0700

    computedproperties vs func and v-for

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <h2>Fullname: {{ fullName }} </h2>
-  <h2>total: {{ total }}</h2>
+  <h2>total computed: {{ total }}</h2>
+  <h2>total method: {{ getTotal() }}</h2>
+  <input type="text" v-model.number="items[1].price">
+
+  <template v-for="item in items" :key="item">
+    <h2 v-if="item.price>100"> {{item.title}} </h2>
+  </template>
+
+  <template v-for="item in expensiveItem" :key="item">
+    <h2> {{item.title}} </h2>
+  </template>
 </template>
 
 <script>
@@ -16,7 +26,7 @@ export default {
         {
           id: 1,
           title: 'TV',
-          price: 100,
+          price: 120,
         },
         {
           id: 2,
@@ -26,20 +36,28 @@ export default {
         {
           id: 3,
           title: 'Laptop',
-          price: 400,
+          price: 40,
         },
-      ]
+      ],
+      country: ''
     };
   },
   methods: {
-    
+    getTotal() {
+      console.log("method");
+      return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    }
   },
   computed: {
     fullName() {
       return `${this.firstName}  ${this.lastName}`
     },
     total() {
+      console.log("computed");
       return this.items.reduce((total, curr) => (total = total + curr.price), 0)
+    },
+    expensiveItem() {
+      return this.items.filter(item => item.price > 100)
     }
   }
 }
