d98316f7aa57c0ae469ee8592ab29266b18a0688
mrtoai38 <toaihuu19122000@gmail.com>
2023-03-30 09:35:50 +0700

    form handling

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,71 @@
 <template>
-    <h2>{{name}}</h2>
-    <div>
-      <button v-on:click="changeName">Change name</button>
+
+    <div> 
+      <pre>
+        {{ JSON.stringify(formValues, null, 2) }}
+      </pre> 
     </div>
-    <h2>{{count}}</h2>
-    <div>
-      <button v-on:click="increment($event), changeName($event)">Increment</button>
-      <button @click="decrement">Decrement</button>
-    </div>
+    <form @submit="submitForm">
+
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="formValues.name">
+      </div>
+
+      <div>
+        <label for="profile">Profile Summary</label>
+        <textarea id="profile" cols="30" rows="10" v-model="formValues.profileSummarry"></textarea>
+      </div>
+
+      <div>
+        <label for="country">Country</label>
+        <select id="country" v-model="formValues.country">
+          <option value="">Select a country</option>
+          <option value="India">Ấn độ</option>
+          <option value="Vietnam">Việt Nam</option>
+          <option value="Singapore">Sing ga po</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="job-location">Job Location</label>
+        <select id="job-location" multiple v-model="formValues.jobLocation">
+          <option value="India">Ấn độ</option>
+          <option value="Vietnam">Việt Nam</option>
+          <option value="Singapore">Sing ga po</option>
+        </select>
+      </div>
+
+      <div>
+        <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
+        <label for="remoteWork">Open to remote work ?</label>
+      </div>
+
+      <div>
+        <label>Skill set</label>
+        <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
+        <label for="html">HTML</label>
+        <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
+        <label for="css">CSS</label>
+        <input type="checkbox" id="js" value="js" v-model="formValues.skillSet"> 
+        <label for="js">Javascript</label>
+      </div>
+
+
+      <div>
+        <label>Years of Experience</label>
+        <input type="radio" id="0-2" value="0-2" v-model="formValues.yearsOfExperience">
+        <label for="0-2">0-2</label>
+        <input type="radio" id="3-5" value="3-5" v-model="formValues.yearsOfExperience">
+        <label for="3-5">3-5</label>
+        <input type="radio" id="6-8" value="6-8" v-model="formValues.yearsOfExperience">
+        <label for="6-8">6-8</label>
+      </div>
+
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
 </template>
 
 <script>
@@ -17,21 +75,21 @@ export default {
   name: 'App',
   data() {
     return {
-      name: 'Toaiben',
-      count: 0,
+      formValues: {
+        name: '',
+        profileSummarry: '',
+        country: '',
+        jobLocation: [],
+        remoteWork: 'no',
+        skillSet: [],
+        yearsOfExperience: ''
+      }
     };
   },
   methods: {
-    changeName(event) {
-      this.name = 'quyetben';
-      console.log('Event c:', event);
-    },
-    increment(event) {
-      this.count++;
-      console.log('Event:', event);
-    },
-    decrement() {
-      this.count--;
+    submitForm(event) {
+      event.preventDefault();
+      console.log('Form Values: ', this.formValues);
     }
   },
 }
@@ -42,9 +100,36 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 
 
